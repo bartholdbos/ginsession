3731302a2742e12a69facccf7486ed013fc2d4da
Barthold Bos <bartholdbos@hotmail.com>
2016-09-07 23:08:54 +0200

    Added Provider and Createmanager code

--- session.go
+++ session.go
@@ -1,6 +1,10 @@
 package ginsession
 
-import ()
+import (
+	"errors"
+)
+
+var providers = make(map[string]Provider)
 
 type Manager struct {
 	name     string
@@ -15,7 +19,19 @@ type Session interface {
 	ID() string                              //Return session ID
 }
 
+type Provider interface {
+	AddSession(ID string) (Session, err) //Create a new session with ID
+	GetSession(ID string) (Session, err) //Get session by ID
+	DelSession(ID string) err            //Delete session by ID
+	ClearSessions(lifetime int64)        //Clear inactive sessions
+}
+
 //Create a new Manager
-func CreateManager(name string, lifetime int64, provider Provider) (*Manager, error) {
-	return nil, nil
+func CreateManager(name string, lifetime int64, providername string) (*Manager, error) {
+	provider, ok := providers[providername]
+	if ok {
+		return &Manager{name: name, lifetime: lifetime, provider: providername}, nil
+	} else {
+		return nil, errors.New("Unknown Provider")
+	}
 }
